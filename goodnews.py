665ff402112b33f68f7e3183a1a1e51fc2077150
aev322 <brian.stascavage@capitalone.com>
2017-04-05 17:36:24 -0400

    Adding initial goodnews method

---
+++ goodnews.py
@@ -0,0 +1,34 @@
+import os, random
+
+from command import Command
+from twitter import *
+from random import randint
+
+class GoodNews(Command):
+    def check_command(self):
+        return { "text": "<%s>" % self.get_news(200), "unfurl_links": True, "unfurl_media": True, "response_type": "in_channel" }
+
+    def get_news(self, count):
+        # Decrypting Twitter keys from Lambda ENV vars
+        ENCRYPTED_ACCESS_KEY = os.environ['twitterAccessKey']
+        access_key = kms.decrypt(CiphertextBlob=b64decode(ENCRYPTED_ACCESS_KEY))['Plaintext']
+        ENCRYPTED_ACCESS_SECRET = os.environ['twitterAccessSecret']
+        access_secret = kms.decrypt(CiphertextBlob=b64decode(ENCRYPTED_ACCESS_SECRET))['Plaintext']
+        ENCRYPTED_CONSUMER_KEY = os.environ['twitterConsumerKey']
+        consumer_key = kms.decrypt(CiphertextBlob=b64decode(ENCRYPTED_CONSUMER_KEY))['Plaintext']
+        ENCRYPTED_CONSUMER_SECRET = os.environ['twitterConsumerSecret']
+        consumer_secret = kms.decrypt(CiphertextBlob=b64decode(ENCRYPTED_CONSUMER_SECRET))['Plaintext']
+        
+        twitter = Twitter(auth = OAuth(access_key, access_secret, consumer_key, consumer_secret))
+        
+        # Getting Godnews twitter user
+        user = os.environ['goodNewsUser']
+
+        try:
+            results = twitter.statuses.user_timeline(screen_name = user, count = count)
+        except Exception as e:
+            self.logger.error("Could not grab Twitter timeline for user: %s" % user)
+            self.logger.error(e)
+
+        return_result = results[randint(0,count-1)]['entities']['media'][0]['expanded_url']
+        return return_result
